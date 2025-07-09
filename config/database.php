cca115661155a0dfd10d4742bc86bba886546a8b
Gabriel Vieira <luizgabrielvieiraleal@id.uff.br>
2025-07-09 10:36:54 -0300

    removendo conexão não utilizada

--- config/database.php
+++ config/database.php
@@ -51,26 +51,6 @@ return [
             ]) : [],
         ],
 
-        'erp' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('ERP_DB_HOST', '127.0.0.1'),
-            'port' => env('ERP_DB_PORT', '3306'),
-            'database' => env('ERP_DB_DATABASE', 'laravel'),
-            'username' => env('ERP_DB_USERNAME', 'root'),
-            'password' => env('ERP_DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ]
-
     ],
 
     /*
